4e989cb364c3c1e3d0abf5d6600943a99fe76f06
jarvis <jarviszhb@163.com>
2024-12-03 23:49:34 +0800

    init: build

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,13 @@ export TZ=Asia/Shanghai
 # 设置项目配置文件的环境变量，如果没有指定则默认使用prod环境
 SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE:-prod}
 
+cd /home/devbox/project/
+
+# maven打包
+mvn clean package -Dmaven.test.skip=true
+
 # 启动应用，game-box-controller是主应用模块
 exec java $JAVA_OPTS \
     -Djava.security.egd=file:/dev/./urandom \
     -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE \
-    -jar /app/game-box-controller/target/game-box-controller.jar
+    -jar /home/devbox/project/game-box-controller/target/game-box-controller-0.0.1-SNAPSHOT.jar
