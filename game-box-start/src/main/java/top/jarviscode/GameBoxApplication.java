b2ea1b1e06928585d7424f68556099d8e045d2f9
剑甫 <hongbozhang.zhb@alibaba-inc.com>
2025-01-24 17:08:42 +0800

    init: 增加start模块

--- game-box-start/src/main/java/top/jarviscode/GameBoxApplication.java
+++ game-box-start/src/main/java/top/jarviscode/GameBoxApplication.java
@@ -1,9 +1,9 @@
-package top.jarviscode.controller;
+package top.jarviscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "top.jarviscode")
 public class GameBoxApplication {
     public static void main(String[] args) {
         SpringApplication.run(GameBoxApplication.class, args);
